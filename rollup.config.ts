b907b622c51fe2babc9bd6ab31281ec6df10df6a
aicherish0320 <aicherish0320@163.com>
2022-04-22 15:05:37 +0800

    rollup config

---
+++ rollup.config.ts
@@ -0,0 +1,27 @@
+import { name } from './package.json'
+import css from 'rollup-plugin-css-only'
+import vue from 'rollup-plugin-vue'
+import typescript from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+const file = (type) => `dist/${name}.${type}.js`
+
+const override = {
+  compilerOptions: { declaration: true },
+  exclude: ['src/main.ts', 'src/App.vue', 'src/main.ts']
+}
+
+export default {
+  input: './src/index.ts',
+  output: {
+    name,
+    file: file('esm'),
+    format: 'es'
+  },
+  plugins: [
+    typescript({ tsconfigOverride: override }),
+    vue(),
+    css({ output: 'bundle.css' }),
+    nodeResolve()
+  ]
+}
